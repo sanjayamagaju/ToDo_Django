ac5efdf63cb68fc0244e27a1ec200674911e7070
sanjaya_magaju <sanjayamagaju.9813126492@gmail.com>
2024-08-14 23:43:04 +0545

    check

--- todolist/settings.py
+++ todolist/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
@@ -94,8 +94,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = "/static/"
 
 
 # Login settings
